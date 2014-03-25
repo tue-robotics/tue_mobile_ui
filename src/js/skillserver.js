dac4a2b0e1e996ce9b7e5a0d12ec858f74a42cdc
Rein Appeldoorn <reinzor@gmail.com>
2014-03-25 18:54:30 +0000

    Added skill server api
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/amigo_mobile_ui@22611 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ src/js/skillserver.js
@@ -0,0 +1,83 @@
+$( document ).ready(function() {
+
+    // Call the skill_server api
+    getSkillServerApi(function (result) {
+
+        var functions = result.functions;
+
+        var cats = getCategories(functions);
+
+        var html = '<table class="buttons">';
+        for (cat in cats) { 
+
+            html += '<tr><td colspan="3" class="table-header">'+cat+'</td></tr>';
+            for ( var i = 0; i < cats[cat].length; i++ ) {
+               
+                if ( i % 3 == 0 ) { 
+                    if ( i > 0 ) {
+                        html += "</tr>";
+                    }
+                    html += "<tr>";                        
+                }
+
+                html += '<td';
+
+                // Last one
+                if ( i == cats[cat].length-1 ) {
+                    var colspan = 3-(i%3);
+                    console.log(colspan);
+                    html += ' colspan="'+colspan+'"';
+                }
+
+                html += '><button type="button" class="btn btn-default" data-src="SkillCommand|'+ cats[cat][i].name +'()">' + cats[cat][i].name + '</button></td>';
+
+            }
+            html += "</tr>";
+
+        }
+        html += "</table>";
+        
+        $('#skillserver').html(html);
+                                
+    });
+
+});
+
+function getSkillServerApi(callback) {
+
+    client = new ROSLIB.Service({
+        ros : ros,
+        name : '/amigo/skill_server/get_api',
+        serviceType : 'robot_skill_server/GetAPI'
+    });
+
+    var req = new ROSLIB.ServiceRequest({});
+
+    client.callService(req, function(result) { callback(result); });
+
+}
+
+function getCategories(functions) {
+
+    var cats = {};
+    
+    for (var i = 0; i < functions.length; i++) {
+
+        var spl = functions[i].name.split(".");
+
+        if (spl.length > 1) {
+            var cat = spl[0];
+        } else {
+            var cat = "amigo";
+        }
+
+        if(!cats[cat]){
+            cats[cat] = [];
+        } 
+        cats[cat].push(functions[i]);
+
+    }
+
+    return cats;
+
+}
