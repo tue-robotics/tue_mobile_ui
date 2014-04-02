26b0fe3dfc7044509bfe8544c294f0e4d2114789
Ramon Wijnands <rayman747@hotmail.com>
2014-04-02 17:11:20 +0000

    merge pingHistory changes
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/amigo_mobile_ui@22943 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/js/ros-connect-amigo.js
+++ src/js/ros-connect-amigo.js
@@ -14,6 +14,8 @@ var rosUrl = 'ws://' + window.location.hostname + ':9090';
 var pingInterval = 5000;  // ms. The time between pings
 var pingTimeout = 2000;     // ms. If ros doesn't respond within this period of time, close the connection
 
+var pingHistory = [];
+
 // global variables
 var ros;
 
@@ -64,6 +66,7 @@ function initConnectionManager() {
   });
 
   ros.addListener('ping.ok', function(e) {
+    pingHistory.push(e);
     console.log('rosbridge ping with %i ms', e);
     modalReconnect.modal('hide');
   });
