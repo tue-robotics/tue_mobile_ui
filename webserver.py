78fd5eeef0c48134702c404c806878354a62a711
Ramon Wijnands <rayman747@hotmail.com>
2014-05-13 19:37:50 +0000

    get index names from the param server
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/amigo_mobile_ui@24255 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- webserver.py
+++ webserver.py
@@ -4,20 +4,21 @@ import rospy
 import sys
 import os
 import roslib
+import socket
 
 os.chdir(roslib.packages.get_pkg_dir('amigo_mobile_ui'))
 
 default_port = 8000
 
 # frist try to create a twisted server
-def createTwistedServer(port):
+def createTwistedServer(port, indexNames):
 	from twisted.web import static, server
 	from twisted.internet import reactor
 
 	print('using TwistedServer')
 
 	root = static.File('.')
-	root.indexNames=['index.html']
+	root.indexNames=indexNames
 	root.contentTypes['.woff'] = 'application/font-woff'
 	factory = server.Site(root)
 
@@ -40,18 +41,22 @@ def createSimpleHTTPServer(port):
 
 if __name__ == '__main__':
 	try:
-		port = rospy.get_param('/amigo_mobile_webserver/port', default_port)
-		print('using port %d from parameter server' % port)
-	except:
-		print('no parameter server running, getting port from argv')
-		if len(sys.argv) > 1 and sys.argv[1].isdigit():
-			port = int(sys.argv[1])
-		else:
-			port = default_port
-
-	try:
+		# rospy.init_node('webserver', anonymous=True)
 		try:
-			createTwistedServer(port)
+			port 	   = rospy.get_param('/amigo_mobile_webserver/port', default_port)
+			indexNames = rospy.get_param('/amigo_mobile_webserver/index_names', ['index.html'])
+			print('using port %d from parameter server' % port)
+			print('using index names: %s from parameter server' % indexNames)
+		except socket.error:
+			print('no parameter server running, getting port from argv')
+			indexNames = ['index.html']
+			if len(sys.argv) > 1 and sys.argv[1].isdigit():
+				port = int(sys.argv[1])
+			else:
+				port = default_port
+
+		try:
+			createTwistedServer(port, indexNames)
 		except ImportError:
 			createSimpleHTTPServer(port)
 	except KeyboardInterrupt:
