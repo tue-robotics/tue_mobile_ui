31857254bb4fd540eca060fea929313fd8a1e66c
Ramon Wijnands <rayman747@hotmail.com>
2014-07-21 21:51:34 +0000

    add swipe detector for the finals
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/amigo_mobile_ui@25761 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/js/final.js
+++ src/js/final.js
@@ -1,17 +1,41 @@
-var img;
+var img, hammertime;
+
 $(document).ready(function () {
-	console.log("loaed");
+  var position = 0;
 
-	img = $('#data-image');
+  img = $('#data-image');
 
-	var listener = new ROSLIB.Topic({
-		ros : ros,
-		name : '/amigo_mobile_gui/image',
-		messageType : 'tue_serialization/Binary'
-	});
-
-	listener.subscribe(function(message) {
-		var data = 'data:image/png;base64,' + message.data;
-		img.prop('src', data);
-	});
+  var listener = new ROSLIB.Topic({
+    ros : ros,
+    name : '/amigo_mobile_gui/image',
+    messageType : 'tue_serialization/Binary'
   });
+
+  listener.subscribe(function(message) {
+    var data = 'data:image/png;base64,' + message.data;
+    img.prop('src', data);
+  });
+
+  var final = $('#final').get(0);
+
+  hammertime = Hammer(final, {
+    drag_max_touches: 1,
+    prevent_default: true
+  });
+
+  hammertime.on('swipe', function (e) {
+    var direction = e.gesture.direction;
+
+    switch (direction) {
+      case 'left': {
+        console.log('left');
+      } break;
+      case 'right': {
+        console.log('right');
+      } break;
+      default: {
+        console.log(e);
+      } break;
+    }
+  });
+});
