857431c0e68247fe743011d254a2c18332119619
Ramon Wijnands <rayman747@hotmail.com>
2014-07-21 15:46:24 +0000

    add word mapping
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/amigo_mobile_ui@25729 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/js/continue.js
+++ src/js/continue.js
@@ -5,6 +5,29 @@ $(document).ready(function () {
 	input = $('input.continue').focus();
 	log   = $('#hear-log');
 
+	var replaceList = [
+		// locations
+		"hallway table",
+		"kitchen counter",
+		"kitchen table",
+		"couch table",
+		"dinner table",
+		"left sidetable",
+		"right sidetable",
+
+		"waste bin",
+
+		// objects
+		"tooth paste",
+		"chocolate milk",
+		"energy drink",
+		"grape juice",
+		"orange juice",
+		"chocolate cookies",
+		"strawberry cookies",
+		"baby food",
+	];
+
 	$('form.continue').on('submit', function (e) {
 		e.preventDefault();
 		var text = input.val();
@@ -14,6 +37,10 @@ $(document).ready(function () {
 		text = text.replace(/[^A-Za-z ]/g, '');
 		text = text.toLowerCase();
 		text = text.replace('  ', ' ');
+
+		$.each(replaceList, function (i, v) {
+			text = text.replace(v, v.replace(/\s/g, ''));
+		});
 
 		var line = $('<li class="list-group-item">Amigo heard: ' + text + '</li>');
 		log.prepend(line);
