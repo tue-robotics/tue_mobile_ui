21679cd30d1d4aabcd2756e2b95c0b882be025d0
Ramon Wijnands <rayman747@hotmail.com>
2014-08-20 20:21:43 +0000

    update qrcode url
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/amigo_mobile_ui@26413 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- utils/qrcode-gen.js
+++ utils/qrcode-gen.js
@@ -1,7 +1,7 @@
 var QRCode = require('qrcode');
 
 var filename = 'qr.png';
-var text = 'http://192.168.2.91:8000/src/';
+var text = 'http://192.168.2.91:8000/';
 
 QRCode.save(filename, text, function (error, written) {
 	if (error) {
