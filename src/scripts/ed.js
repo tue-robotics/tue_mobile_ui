14729ad211c1c9f6c28c76613d16c1d3fefe5280
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-09-26 12:52:57 +0200

    Put ed camera in seperate js file

--- src/scripts/ed.js
+++ src/scripts/ed.js
@@ -10,231 +10,7 @@ SceneJS.setConfigs({
 var scene;
 var clientQueryMeshes;
 
-/**
- * Orbiting camera node type
- *
- * Usage example
- * -------------
- *
- * someNode.addNode({
- *      type: "cameras/orbit",
- *      eye:{ x: y:0 },
- *      look:{ y:0 },
- *      yaw: 340,,
- *      pitch: -20,
- *      zoom: 350,
- *      zoomSensitivity:10.0,
- * });
- *
- * The camera is initially positioned at the given 'eye' and 'look', then the distance of 'eye' is zoomed out
- * away from 'look' by the amount given in 'zoom', and then 'eye' is rotated by 'yaw' and 'pitch'.
- *
- */
-SceneJS.Types.addType("ed_camera", {
-
-    construct:function (params) {
-
-        var lookat = this.addNode({
-            type:"lookAt",
-
-            // A plugin node type is responsible for attaching specified
-            // child nodes within itself
-            nodes:params.nodes
-        });
-
-        var yaw = params.yaw || 0;
-        var pitch = params.pitch || 0;
-        var zoom = params.zoom || 10;
-        var minPitch = params.minPitch;
-        var maxPitch = params.maxPitch;
-        var zoomSensitivity = params.zoomSensitivity || 1.0;
-
-        var lastX;
-        var lastY;
-        var dragging = false;
-
-        var eye = params.eye || { x:0, y:0, z:0 };
-        var look = params.look || { x:0, y:0, z:0};
-
-        lookat.set({
-            eye:{ x:eye.x, y:eye.y, z:-zoom },
-            look:{ x:look.x, y:look.y, z:look.z },
-            up:{ x:0, y:1, z:0 }
-        });
-
-        update();
-
-        var canvas = this.getScene().getCanvas();
-
-        canvas.addEventListener('mousedown', mouseDown, true);
-        canvas.addEventListener('mousemove', mouseMove, true);
-        canvas.addEventListener('mouseup', mouseUp, true);
-        canvas.addEventListener('touchstart', touchStart, true);
-        canvas.addEventListener('touchmove', touchMove, true);
-        canvas.addEventListener('touchend', touchEnd, true);
-        canvas.addEventListener('mousewheel', mouseWheel, true);
-        canvas.addEventListener('DOMMouseScroll', mouseWheel, true);
-
-        function mouseDown(event) {
-            lastX = event.clientX;
-            lastY = event.clientY;
-            dragging = true;
-        }
-
-        function touchStart(event) {
-            lastX = event.targetTouches[0].clientX;
-            lastY = event.targetTouches[0].clientY;
-            dragging = true;
-        }
-
-        function mouseUp() {
-            dragging = false;
-        }
-
-        function touchEnd() {
-            dragging = false;
-        }
-
-        function mouseMove(event) {
-            var posX = event.clientX;
-            var posY = event.clientY;
-            actionMove(posX, posY);
-        }
-
-        function touchMove(event) {
-            var posX = event.targetTouches[0].clientX;
-            var posY = event.targetTouches[0].clientY;
-            actionMove(posX, posY);
-        }
-
-        function actionMove(posX, posY) {
-            if (dragging) {
-
-                yaw -= (posX - lastX) * 0.1;
-                pitch -= (posY - lastY) * 0.1;
-
-                update();
-
-                lastX = posX;
-                lastY = posY;
-            }
-        }
-
-        function mouseWheel(event) {
-            var delta = 0;
-            if (!event) event = window.event;
-            if (event.wheelDelta) {
-                delta = event.wheelDelta / 120;
-                if (window.opera) delta = -delta;
-            } else if (event.detail) {
-                delta = -event.detail / 3;
-            }
-            if (delta) {
-                if (delta < 0) {
-                    zoom -= zoomSensitivity;
-                } else {
-                    zoom += zoomSensitivity;
-                }
-            }
-            if (event.preventDefault) {
-                event.preventDefault();
-            }
-            event.returnValue = false;
-            update();
-
-        }
-
-        function update() {
-
-            if (minPitch != undefined && pitch < minPitch) {
-                pitch = minPitch;
-            }
-
-            if (maxPitch != undefined && pitch > maxPitch) {
-                pitch = maxPitch;
-            }
-
-            var eye = [0, 0, zoom];
-            var look = [0, 0, 0];
-            var up = [0, 1, 0];
-
-            // TODO: These references are to private SceneJS math methods, which are not part of API
-
-            var eyeVec = SceneJS_math_subVec3(eye, look, []);
-            var axis = SceneJS_math_cross3Vec3(up, eyeVec, []);
-
-            var pitchMat = SceneJS_math_rotationMat4v(pitch * 0.0174532925, axis);
-            var yawMat = SceneJS_math_rotationMat4v(yaw * 0.0174532925, up);
-
-            var eye3 = SceneJS_math_transformPoint3(pitchMat, eye);
-            eye3 = SceneJS_math_transformPoint3(yawMat, eye3);
-
-            lookat.setEye({x:eye3[0], y:eye3[1], z:eye3[2] });
-        }
-    },
-
-    setLook: function(l) {
-
-
-    },
-
-    destruct:function () {
-        // TODO: remove mouse handlers
-    }
-});
-
-function calculateNormals (vertices, triangles) {
-
-    var normals = new Float32Array(vertices.length);
-
-    for(var i = 0; i < vertices.length; i += 3) {
-      normals[i] = 1;
-      normals[i + 1] = 0;
-      normals[i + 2] = 0;
-    }
-
-    // for (var i = 0; i < indices.length; i++) {
-    //     var j0 = indices[i][0];
-    //     var j1 = indices[i][1];
-    //     var j2 = indices[i][2];
-
-    //     var v1 = positions[j0];
-    //     var v2 = positions[j1];
-    //     var v3 = positions[j2];
-
-    //     v2 = SceneJS_math_subVec4(v2, v1, [0, 0, 0, 0]);
-    //     v3 = SceneJS_math_subVec4(v3, v1, [0, 0, 0, 0]);
-
-    //     var n = SceneJS_math_normalizeVec4(SceneJS_math_cross3Vec4(v2, v3, [0, 0, 0, 0]), [0, 0, 0, 0]);
-
-    //     if (!nvecs[j0]) nvecs[j0] = [];
-    //     if (!nvecs[j1]) nvecs[j1] = [];
-    //     if (!nvecs[j2]) nvecs[j2] = [];
-
-    //     nvecs[j0].push(n);
-    //     nvecs[j1].push(n);
-    //     nvecs[j2].push(n);
-    // }
-
-    // var normals = new Array(positions.length);
-
-    // // now go through and average out everything
-    // for (var i = 0; i < nvecs.length; i++) {
-    //     var count = nvecs[i].length;
-    //     var x = 0;
-    //     var y = 0;
-    //     var z = 0;
-    //     for (var j = 0; j < count; j++) {
-    //         x += nvecs[i][j][0];
-    //         y += nvecs[i][j][1];
-    //         z += nvecs[i][j][2];
-    //     }
-    //     normals[i] = [-(x / count), -(y / count), -(z / count)];
-    // }
-    return normals;
-}
-
-var REDS = [0.8, 0  , 0.8, 0,   0.8, 0  , 0.8];
+var REDS =   [ 0.8, 0  , 0.8, 0,   0.8, 0  , 0.8];
 var GREENS = [ 0.8, 0.8, 0  , 0,   0.8, 0.8, 0  ];
 var BLUES =  [ 0.8, 0.8, 0.8, 0.8, 0,   0,   0  ];
 
@@ -253,22 +29,15 @@ function djb2(str){
 }
 
 function handleMeshQueryResult(msg) {
-  // console.log(entity_ids)
 
   for (var i = 0; i < msg.entity_ids.length; i++) {
     var id = msg.entity_ids[i]
-    var n = scene.getNode(id);
-
-    // Remove the mesh ...
-    n.removeNode(id + "-mesh");
 
     // Generate color from hash function
     var iColor = djb2(id) % REDS.length;
     var r = REDS[iColor];
     var g = GREENS[iColor];
     var b = BLUES[iColor];
-
-    console.log(id + " " + r + " " + g + " " + b + " ");
 
     indices = new Uint16Array(msg.meshes[i].vertices.length / 3);
     for (var j = 0; j < indices.length; j++) {
@@ -302,8 +71,11 @@ function handleMeshQueryResult(msg) {
       normals[j + 8] = n[2];
     }
 
-    // Why do I have to do it again?
+    // Get the entity scene node
     var n = scene.getNode(id);
+
+    // Remove the mesh ...
+    n.removeNode(id + "-mesh");
 
     // ... and replace it by the received one
     n.addNode(
@@ -410,11 +182,12 @@ $(document).ready(function () {
         up:{ x:0, y:0, z:1 },
 
         nodes:[
-
-          // Default material
           {
-            type:"material",
-            color:{ r:0.3, g:0.3, b:1.0 },
+            type:"matrix",
+            elements: [1, 0, 0, 0,
+                       0, 1, 0, 0,
+                       0, 0, 1, 0,
+                       0, 0, 0, 1],
             id: "world"
           }
         ]
