e97da7a2256d19a2ca95e68185f61ae04450dbfd
Ramon Wijnands <rayman747@hotmail.com>
2015-06-16 22:13:55 +0200

    fixup! scaffold a new angular app

--- challenge_open/app/scripts/app.js
+++ challenge_open/app/scripts/app.js
@@ -11,20 +11,5 @@
 angular
   .module('challengeOpenApp', [
     'ngAnimate',
-    'ngRoute',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  ]);
