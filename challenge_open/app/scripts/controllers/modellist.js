8e733a36f595db4ec732a63ed011072dce3eae72
Ramon Wijnands <rayman747@hotmail.com>
2015-07-16 05:11:12 +0200

    fix indentation

--- challenge_open/app/scripts/controllers/modellist.js
+++ challenge_open/app/scripts/controllers/modellist.js
@@ -2,7 +2,7 @@
 
 angular.module('challengeOpenApp')
   .controller('ModellistCtrl', function ($scope, robot) {
-	robot.ed.updateModels(); //tmp
+    robot.ed.updateModels(); //tmp
     $scope.models = robot.ed.models;
 
     robot.ed.on('models', function (models) {
