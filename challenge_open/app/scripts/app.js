7acbae69f73a4b6a324fbdb06612ba22a2619a7e
Rein Appeldoorn <reinzor@gmail.com>
2015-07-17 07:28:32 +0200

    added tap faster reponse and fixed body and html

--- challenge_open/app/scripts/app.js
+++ challenge_open/app/scripts/app.js
@@ -14,3 +14,27 @@ angular
     // 'ngTouch',
     'ngDraggable',
   ]);
+
+angular.module('challengeOpenApp').directive("ngTap", function() {
+  return function($scope, $element, $attributes) {
+    var tapped;
+    tapped = false;
+    $element.bind("click", function() {
+      if (!tapped) {
+        return $scope.$apply($attributes["ngTap"]);
+      }
+    });
+    $element.bind("touchstart", function(event) {
+      return tapped = true;
+    });
+    $element.bind("touchmove", function(event) {
+      tapped = false;
+      return event.stopImmediatePropagation();
+    });
+    return $element.bind("touchend", function() {
+      if (tapped) {
+        return $scope.$apply($attributes["ngTap"]);
+      }
+    });
+  };
+});
