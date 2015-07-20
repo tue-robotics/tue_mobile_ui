5d5139993a0dc2fa5df52863c5db2e41fc6867d3
Ramon Wijnands <rayman747@hotmail.com>
2015-07-20 04:13:05 +0200

    fix indentation

--- challenge_open/app/scripts/run.js
+++ challenge_open/app/scripts/run.js
@@ -2,73 +2,73 @@
 
 angular.module('challengeOpenApp')
   .run(function() {
+    /* global FastClick */
     FastClick.attach(document.body);
 
     var initialY = null;
     document.ontouchstart = function(e) {
-    	initialY = e.pageY;
-    }
+      initialY = e.pageY;
+    };
 
     document.ontouchend = function(e) {
-    	initialY = null;
-    }
+      initialY = null;
+    };
 
     document.ontouchcancel = function(e) {
-    	initialY = null;
-    }
+      initialY = null;
+    };
 
     function inModels(node) {
-    	var in_scroller = false;
-    	while (node != document) {
-    		if (node.classList.contains("models")) {
-    			return true;
-    		}
-    		node = node.parentNode;
-    	}
-    	return false;
-	}
+      var in_scroller = false;
+      while (node !== document) {
+        if (node.classList.contains("models")) {
+          return true;
+        }
+        node = node.parentNode;
+      }
+      return false;
+    }
 
-	function inSnapshots(node) {
-    	var in_scroller = false;
-    	while (node != document) {
-    		if (node.classList.contains("snapshots")) {
-    			return true;
-    		}
-    		node = node.parentNode;
-    	}
-    	return false;
-	} 
+    function inSnapshots(node) {
+        var in_scroller = false;
+        while (node !== document) {
+          if (node.classList.contains("snapshots")) {
+            return true;
+          }
+          node = node.parentNode;
+        }
+        return false;
+    }
 
     document.ontouchmove = function(e) {
-	    if(initialY !== null) {
-	        if(!inModels(e.target) && !inSnapshots(e.target)) {
-	            // The element to be scrolled is not the content node
-	            e.preventDefault();
-	            return;
-	        }
+      if(initialY !== null) {
+          if(!inModels(e.target) && !inSnapshots(e.target)) {
+              // The element to be scrolled is not the content node
+              e.preventDefault();
+              return;
+          }
 
-	        var direction   = e.pageY - initialY;
+          var direction   = e.pageY - initialY;
 
-	        // modelsNode
-	        var modelsNode = document.getElementsByClassName("models")[0]
+          // modelsNode
+          var modelsNode = document.getElementsByClassName("models")[0];
 
-	        if (inSnapshots(e.target))
-	        {
-	        	if (abs(direction) < 3) {	
-	        	}	
-	        	else {
-	        		e.preventDefault();
-	        	}        	
-	        }
+          if (inSnapshots(e.target)) {
+            if (abs(direction) < 3) {
 
-	        if(direction > 0 && modelsNode.scrollTop <= 0) {
-	            // The user is scrolling up, and the element is already scrolled to top
-	            e.preventDefault();
-	        } else if(direction < 0 && modelsNode.scrollTop >= modelsNode.scrollHeight - modelsNode.clientHeight) {
-	            // The user is scrolling down, and the element is already scrolled to bottom
-	            e.preventDefault();
-	        }
-	    }
-	}
+            } else {
+              e.preventDefault();
+            }
+          }
+
+          if(direction > 0 && modelsNode.scrollTop <= 0) {
+              // The user is scrolling up, and the element is already scrolled to top
+              e.preventDefault();
+          } else if(direction < 0 && modelsNode.scrollTop >= modelsNode.scrollHeight - modelsNode.clientHeight) {
+              // The user is scrolling down, and the element is already scrolled to bottom
+              e.preventDefault();
+          }
+      }
+    };
 
   });
