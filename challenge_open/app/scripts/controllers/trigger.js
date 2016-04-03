d9d5dc78e1ec1efa53a5e6de8babde42428a8820
Ramon Wijnands <rayman747@hotmail.com>
2016-04-03 11:55:47 +0200

    add amigo trigger

--- challenge_open/app/scripts/controllers/trigger.js
+++ challenge_open/app/scripts/controllers/trigger.js
@@ -10,9 +10,21 @@ angular.module('challengeOpenApp')
       messageType: 'std_msgs/String',
     });
 
+    var amigoTopic = robot.ros.Topic({
+      name: '/amigo/trigger',
+      messageType: 'std_msgs/String',
+    });
+
     $scope.trigger = function (name) {
       console.log('trigger:', name);
       topic.publish({
+        data: name,
+      });
+    };
+
+    $scope.triggerAmigo = function (name) {
+      console.log('triggerAmigo:', name);
+      amigoTopic.publish({
         data: name,
       });
     };
