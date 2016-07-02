d267f3ed9959b92e563f67243e754cd62b875d7f
Ramon Wijnands <rayman747@hotmail.com>
2016-07-02 19:35:08 +0200

    make the cocktail gui

--- cocktail_gui/app/scripts/main.js
+++ cocktail_gui/app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+var robot = window.r = new API.Robot();
+robot.connect();
+
+var topic = robot.ros.Topic({
+  name: 'trigger',
+  messageType: 'std_msgs/String',
+});
+
+var locs = location.search.split('=');
+var table = locs[1] || '?';
+
+var button = document.getElementById('call');
+button.addEventListener('click', function (e) {
+  console.log('order for table:', table);
+  topic.publish({
+    data: 'take order for table' + table,
+  });
+});
+
+button.innerText = table;
