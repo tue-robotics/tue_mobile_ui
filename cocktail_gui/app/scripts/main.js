6831a7e4daade1fa81807e39d37b8b3d951a6253
Ramon Wijnands <rayman747@hotmail.com>
2016-07-02 21:37:51 +0200

    finish the cocktail app

--- cocktail_gui/app/scripts/main.js
+++ cocktail_gui/app/scripts/main.js
@@ -7,7 +7,7 @@ var topic = robot.ros.Topic({
 });
 
 var locs = location.search.split('=');
-var table = locs[1] || '?';
+var table = locs[1] ? locs[1][0]: '?';
 
 var button = document.getElementById('call');
 button.addEventListener('click', function (e) {
@@ -15,6 +15,12 @@ button.addEventListener('click', function (e) {
   topic.publish({
     data: 'take order for table' + table,
   });
+  button.disabled = true;
+  button.classList.add('btn-primary');
+  setTimeout(function () {
+    button.classList.remove('btn-primary');
+    button.disabled = false;
+  }, 500);
 });
 
 button.innerText = table;
