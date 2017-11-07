3853bd2e101f81ac2f4efca0b344957ad6df8d5d
Janno Lunenburg <jannolunenburg@gmail.com>
2017-11-07 20:59:17 +0100

    feat(Gripper)Gripper goals working

--- ed/app/scripts/controllers/jointcontroller.js
+++ ed/app/scripts/controllers/jointcontroller.js
@@ -4,49 +4,16 @@ angular.module('EdGuiApp')
   .controller('JointCtrl', function($scope, $timeout, robot) {
 
     $scope.sendJointCommand = function(cmd) {
-      console.log('Joint command:', cmd)
+      console.log('Joint command: ', cmd)
       robot.body.sendGoal(cmd)
+    }
+
+    $scope.sendGripperCommand = function(cmd) {
+      console.log('Gripper command: ', cmd)
+      robot.body.sendGripperGoal(cmd)
     }
 
     $scope.joint_names = robot.body.joint_names
     $scope.default_configurations = robot.body.default_configurations
 
   });
-
-//   function handleJointState(device,name_arguments,position_arguments) {
-//
-//   /* parse argument array */
-//   position_arguments = position_arguments.split(',');
-//   for (var i=0; i<position_arguments.length; i++) {
-//     position_arguments[i] = parseFloat(position_arguments[i]);
-//   }
-//
-//   /* Joint names */
-//   name_arguments = name_arguments.split(',');
-//
-//
-//   console.log('device: ', device);
-//   console.log('name_arguments: ', name_arguments);
-//   console.log('position_arguments: ', position_arguments);
-//
-//   var action = new ROSLIB.ActionClient({
-//     ros: ros,
-//     serverName: 'body/joint_trajectory_action',
-//     actionName: 'control_msgs/FollowJointTrajectoryAction',
-//     timeout: 10,
-//   });
-//
-//   var goal = new ROSLIB.Goal({
-//     actionClient: action,
-//     goalMessage: {
-//       trajectory: {
-//         joint_names: name_arguments,
-//         points: [{
-//           positions: position_arguments
-//         }]
-//       }
-//     }
-//   });
-//
-//   goal.send();
-// }
