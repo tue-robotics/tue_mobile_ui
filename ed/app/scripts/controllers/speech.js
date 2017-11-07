863d1ac7115a89eee592969edefd30b747dd8100
Janno Lunenburg <jannolunenburg@gmail.com>
2017-11-07 23:06:21 +0100

    feat(Speech)History working

--- ed/app/scripts/controllers/speech.js
+++ ed/app/scripts/controllers/speech.js
@@ -3,9 +3,34 @@
 angular.module('EdGuiApp')
   .controller('SpeechCtrl', function($scope, $timeout, robot) {
 
-    $scope.speak = function() {
-      console.log('Speak command: ', $scope.sentence)
-      robot.speech.speak({sentence: $scope.sentence})
-    }
+    $scope.speech_history = []
+
+    $scope.sentence = ""
+
+    $scope.speak = function(sentence) {
+      console.log('Speak command: ', sentence)
+      // Forward the speech command to the robot api
+      robot.speech.speak({sentence: sentence})
+
+      // Clear the input box
+      $scope.sentence = ''
+
+      // Remember lines
+      $scope.speech_history.unshift(sentence)
+
+      // Filter out duplicates
+      var unique = [];
+      var lines = $scope.speech_history.filter(function (text) {
+
+        if (unique.indexOf(text) === -1) {
+          unique.push(text);
+          // return true;
+        } else {
+          // return false;
+        }
+      });
+      $scope.speech_history = unique;
+
+  }  // End of speak
 
   });
