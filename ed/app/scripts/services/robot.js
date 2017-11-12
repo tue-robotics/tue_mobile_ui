9a0550797633953ad3dbf2404b73d8b9652c1a3d
Ramon Wijnands <rayman747@hotmail.com>
2017-11-12 23:34:18 +0100

    Cherry-pick robot.js from e66e0cc

--- ed/app/scripts/services/robot.js
+++ ed/app/scripts/services/robot.js
@@ -12,7 +12,14 @@ angular.module('EdGuiApp')
 
     // Method for instantiating
     this.$get = function () {
-      var robot = window.r = new API.Robot();
+      var robot = window.r = new API.Robot({
+        'ed': true,
+        'hardware': true,
+        'head': true,
+        'base': true,
+        'body': true,
+        'speech': true,
+        'actionServer': true});
       robot.connect();
       return robot;
     };
