d0cebf895171473412c6a520cfeed6a13c33cb82
Janno Lunenburg <jannolunenburg@gmail.com>
2017-11-28 23:00:49 +0100

    wip(Actions)Cancelling working, disabling as well, needs some finetuning

--- ed/app/scripts/controllers/actions.js
+++ ed/app/scripts/controllers/actions.js
@@ -9,6 +9,13 @@ angular.module('EdGuiApp')
       messageType: 'std_msgs/String',
     });
 
+    // Action client
+    var actionClient = new ROSLIB.ActionClient({
+      ros: robot.ros,
+      serverName: 'action_server/task',
+      actionName: 'action_server/TaskAction',
+    });
+
   	// Trigger button callback
     $scope.trigger = function (name) {
       console.log('trigger:', name);
@@ -17,20 +24,20 @@ angular.module('EdGuiApp')
       });
     };
 
-    // Action
-    var action = new ROSLIB.ActionClient({
-      ros: robot.ros,
-      serverName: 'action_server/task',
-      actionName: 'action_server/TaskAction',
-    });
+    // Callback function for cancel button
+    $scope.cancel = function() {
+    	console.log("Canceling current action");
+    	actionClient.cancel();
+    }
+
 
     // Callback function to perform autonomous presentation
     $scope.present = function (language) {
-      console.log('Starting presentation in ', language)
+      console.log('Starting presentation in', language)
 
    	  // Check if language is 'en' or 'nl'
       if (language != 'en' && language != 'nl') {
-      	console.error('Language should be "en" or "nl", now it is ', language);
+      	console.error('Language should be "en" or "nl", now it is', language);
       	return;
       }
 
@@ -44,10 +51,32 @@ angular.module('EdGuiApp')
 
       // Create actionlib goal
       var goal = new ROSLIB.Goal({
-        actionClient: action,
+        actionClient,
         goalMessage: {
           recipe: JSON.stringify(recipe)
         }
+      });
+
+      // Add feedback callback
+      goal.on('feedback', function(feedback) {
+        console.log('Receiving feedback');
+        // ToDo Rokus: publish more often
+        // console.log('Feedback: ' + feedback.current_subtask);
+      });
+
+      // Add status callback
+      goal.on('status', function(status) {
+      	if (status.status == 1)
+      	{
+      		$scope.active = true;
+      		// $scope.idle = false;
+      	} else {
+      		$scope.active = false;
+      		// $scope.idle = true;
+      	}
+      	$scope.idle = false;  // Set idle to true by default to make sure external actions can be cancelled
+      	// console.log('Receiving status');
+        // console.log('Feedback: ' + feedback.current_subtask);
       });
 
       // Send goal
@@ -55,4 +84,4 @@ angular.module('EdGuiApp')
     
     };  // End of 'present' 
 
-});
+});  // End of ActionsCtrl
