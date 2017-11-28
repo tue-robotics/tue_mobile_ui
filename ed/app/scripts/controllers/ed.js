c5f407afb8b29640030359cf30ef75bb439a42b2
Janno Lunenburg <jannolunenburg@gmail.com>
2017-11-28 20:29:26 +0100

    feat(Ed)Added ED reset button

---
+++ ed/app/scripts/controllers/ed.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('EdGuiApp')
+  .controller('EdCtrl', function($scope, $timeout, robot) {
+
+  	$scope.reset = function() {
+      console.log('Reset ED');
+      robot.ed.reset(true);
+    }
+
+});
