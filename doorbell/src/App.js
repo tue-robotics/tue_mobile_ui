32bf66ece9561d4d929ad07f71eb123e7fe5ce35
Ramon Wijnands <rayman747@hotmail.com>
2018-04-28 15:07:16 +0200

    Implement doorbell gui

--- doorbell/src/App.js
+++ doorbell/src/App.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Topic, Ros } from 'roslib';
+import Doorbell from './Doorbell';
 import './App.css';
+import { hostname } from 'os';
+
+const RECONNECT_TIMEOUT = 2000;
+
+// Private variables
+const host = hostname() || 'localhost';
+const defaultUrl = `ws://${host}:9090`;
 
 class App extends Component {
+  componentWillMount() {
+    this.ros = new Ros({
+      encoding: 'ascii',
+      url: defaultUrl,
+    });
+
+    // reconnect behavior
+    this.ros.on('error', () =>
+      setTimeout(() => this.ros.connect(defaultUrl), RECONNECT_TIMEOUT),
+    );
+
+    this.ros.on('connection', () => {
+      console.log('connection');
+    });
+
+    this.topic = this.ros.Topic({
+      name: 'trigger',
+      messageType: 'std_msgs/String',
+    });
+  }
+
+  handleClick = () => {
+    console.log('this is:', this);
+    this.topic.publish({
+      data: 'doorbell',
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Doorbell onClick={this.handleClick} />
         </p>
       </div>
     );
