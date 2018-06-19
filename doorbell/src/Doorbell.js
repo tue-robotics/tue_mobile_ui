f471ed2d0452559836d638c667c21b6e34d1b509
Ramon Wijnands <rayman747@hotmail.com>
2018-06-19 15:52:24 -0400

    Fix doorbell on mobile

--- doorbell/src/Doorbell.js
+++ doorbell/src/Doorbell.js
@@ -4,9 +4,7 @@ import "./Doorbell.css"
 class Doorbell extends Component {
   render() {
     return (
-      <button className="doorbell" {...this.props}>
-        <img src="doorbell.png" alt="Doorbell" />
-      </button>
+      <button className="doorbell" {...this.props}></button>
     );
   }
 }
