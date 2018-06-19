758fc285497ca89a719d62022011ae633cbcfdc1
Ramon Wijnands <rayman747@hotmail.com>
2018-06-19 15:44:49 -0400

    Scale the doorbell button (fix for mobile?)

--- doorbell/src/Doorbell.js
+++ doorbell/src/Doorbell.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import "./Doorbell.css"
 
 class Doorbell extends Component {
   render() {
     return (
-      <button {...this.props}>
+      <button className="doorbell" {...this.props}>
         <img src="doorbell.png" alt="Doorbell" />
       </button>
     );
