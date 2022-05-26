9edb58a10a6de0727d68e70091adcdb1838b7e20
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-05-26 16:10:00 +0200

    (ed) fix no-redeclare lint issues

--- ed/app/scripts/orbitControls.js
+++ ed/app/scripts/orbitControls.js
@@ -5,6 +5,7 @@
  * @author WestLangley / http://github.com/WestLangley
  * @author erich666 / http://erichaines.com
  */
+/*eslint no-redeclare: ["error", { "builtinGlobals": false }]*/
 /*global THREE, console */
 
 (function () {
